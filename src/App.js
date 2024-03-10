c8124f647b110fb616b3c1aeca45b08c2e40e9fb
User <emmyaondohemba45@gmail.com>
2024-03-10 17:00:11 +0100

    home and form page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Form } from "./form/Form";
 import { Success } from "./succes/Success";
+import Home from "./home/Home";
 function App() {
 
   return (
   <div>
     <Routes>
-      <Route exact path="/" element={<Form />} />
+      <Route exact path="/" element={<Home />} />
+      <Route path="/application-form" element={<Form />} />
       <Route path="/success" element={<Success />} />
     </Routes>
   </div>
