abf7ef898fe25ccb57958d7718f282e14c600b20
brave-source <eebiyenrin@gmail.com>
2024-03-17 20:30:18 +0100

    Move success to component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Form } from "./components/form/Form";
-import { Success } from "./succes/Success";
+import { Success } from "./components/succes/Success";
 import Home from "./components/home/Home";
 import Hero from "./components/hero/hero";
 function App() {
